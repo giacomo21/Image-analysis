98702be6a008e2fde445475c5640dba97a903ccc
Nikhil96 <nikhilkotha199623@gmail.com>
2013-07-13 15:48:58 +0200

    litaf

--- nikhil_litafnucleus.py
+++ nikhil_litafnucleus.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+#/home/nikhil/Desktop/FBK Project/Alisi Data/Stellate/AC_HSC_CTRL/03_LITAF.tif
 from   PIL                   import Image
 from   scipy                 import ndimage
 from   skimage               import data
@@ -11,8 +11,9 @@ import pymorph
 import mahotas
 import scipy
 import struct
-
 import nikhil_lib
+#import nikhil_load
+#y = nikhil_load
 z = nikhil_lib
 
 a = z.loadslice()
@@ -20,5 +21,26 @@ a = z.loadslice()
 #print a[0].mean()
 
 plt.imshow(a[1], cmap = plt.get_cmap('gray'))
+#plt.show()
+
+nucleus = a[1]
+threshold = mahotas.otsu(nucleus)
+print threshold 
+
+nucleus[nucleus < threshold] = 0
+nucleus[nucleus > threshold] = 255
+
+#plt.imshow(nucleus)
+#plt.show()
+
+binarynucleus = pymorph.binary(nucleus)
+fillnuc = scipy.ndimage.morphology.binary_fill_holes(binarynucleus)
+fillnuc = fillnuc.astype(np.uint8)
+fillnuc[fillnuc > 0] = 255
+
+
+print fillnuc
+plt.imshow(fillnuc)
 plt.show()
+
 
