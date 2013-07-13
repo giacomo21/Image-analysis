d4decc69803727aac6dc07a23bb923138df1367a
Giacomo Inzani <giacomo.inzani@gmail.com>
2013-07-13 18:08:49 +0200

    asdf

--- analysis.py
+++ analysis.py
@@ -33,3 +33,9 @@ def litafnucleus(nucleus, litaf):
 	regi = selectregion(litaf, nu)
 	return(regi)
 #
+
+def histog(A, B):
+	NA = plt.hist(A, bins=255, alpha=0.5, color='b')
+	AC = plt.hist(B, bins=255, alpha=0.5, color='r')
+	plt.show()
+#
