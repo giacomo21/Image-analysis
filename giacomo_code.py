fd54a195debdfc59e38614103e149e4ba8d44f35
Giacomo Inzani <giacomo.inzani@gmail.com>
2013-07-13 15:29:01 +0200

    asdf

--- giacomo_code.py
+++ giacomo_code.py
@@ -1,22 +1,17 @@
-#!/usr/bin/env python
-
 import matplotlib.pyplot as plt	# import libraries
 import matplotlib.cm as cm
 import scipy.ndimage
 
 cmap = cm.Greys_r
 
-import marco_lib
-import giacomo_analysis.py
+import marco_load
+import giacomo_analysis
 
 temp = ['03_LITAF.tif','03_nucleo.tif']
-tempdata = marco_lib.loadslice(temp)
+tempdata = marco_load.loadslice(temp)
 
 plt.imshow(tempdata[0], cmap)
 plt.show()
 
 litafselected = giacomo_analysis.analysis(tempdata[0], cmap)
-
-print litafselected
-
 
