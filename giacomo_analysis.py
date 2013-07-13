edba9a5f3bfb112387d4239c662f10c086e5688a
Giacomo Inzani <giacomo.inzani@gmail.com>
2013-07-13 16:16:39 +0200

    asdf

--- giacomo_analysis.py
+++ giacomo_analysis.py
@@ -2,6 +2,9 @@ import matplotlib.pyplot as plt
 import mahotas
 import scipy.ndimage
 import numpy
+import matplotlib.cm as cm
+
+cmap = cm.Greys_r
 
 def analysis(A, B):
 	global filled
@@ -9,8 +12,12 @@ def analysis(A, B):
 	A[ A<T ] = 0
 	A[ A>=T ] = 255
 	C = A / 255
+	plt.imshow(A, cmap)
+	plt.show()
 	filled = scipy.ndimage.morphology.binary_fill_holes(C)
 	filled = filled.astype(numpy.uint8)
 	filled[filled>0]=255
+	plt.imshow(filled, cmap)
+	plt.show()
 	selection = B[filled>0]
 	return(selection)
--- giacomo_code.py
+++ giacomo_code.py
@@ -9,13 +9,13 @@ import marco_load
 import giacomo_analysis
 import giacomo_average
 
-temp = ['03_LITAF.tif','03_nucleo.tif']
+temp = ['03_nucleo.tif', '03_LITAF.tif']
 tempdata = marco_load.loadslice(temp)
 
 plt.imshow(tempdata[0], cmap)
 plt.show()
 
-litafselected = giacomo_analysis.analysis(tempdata[0], cmap)
+litafselected = giacomo_analysis.analysis(tempdata[0], tempdata[1])
 
 litafnucleusaverage = giacomo_average.averageintens(litafselected) 
 
