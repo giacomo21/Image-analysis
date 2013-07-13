64da9ece4f9a4920edcfd08fed3a9e8cd48fda82
Giacomo Inzani <giacomo.inzani@gmail.com>
2013-07-13 10:52:57 +0200

    i've added the stuff to select channels

--- Iman.py
+++ Iman.py
@@ -1,3 +1,5 @@
+#Import a single image
+
 import matplotlib.pyplot as plt
 
 import numpy as np
@@ -5,3 +7,17 @@ import numpy as np
 orig = plt.imread('image.png')
 
 ndarr = np.array(orig)
+
+# Import a slice of images
+
+# Select the Red channel
+
+R = [:, :, 0]
+
+# Select the Green channel
+
+G = [:, :, 1]
+
+# Select the Blue channel
+
+B = [:, :, 2]
