34e6769f1c40072a82732b8ca3fc75ef210e2405
Giacomo Inzani <giacomo.inzani@gmail.com>
2013-07-13 11:43:08 +0200

    new load slices

---
+++ loadslice.py
@@ -0,0 +1,7 @@
+def loadslice(nuclei, litaf):		# loading two images
+	nucleiarr = plt.imread(nuclei)
+	litafarr = plt.imread(litaf)
+	Wnucleiarr = nucleiarr[255, 255, 255]	# selecting the white channel for the nuclei images
+	Rlitafarr = litafarr[:,:,0]	# selecting the red channel for the litaf images
+	RWarr = [Wnucleiarr, Rlitafarr]
+	
