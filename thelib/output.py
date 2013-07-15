23138b6bcad99e31d27ba24d548c2314b9781d24
Federica Annecchini <fede.anne95@hotmail.it>
2013-07-15 22:29:43 +0200

    ok Merge branch 'master' of github.com:giacomo21/Image-analysis

--- thelib/output.py
+++ thelib/output.py
@@ -7,9 +7,16 @@ def plot(data):
 	plt.show()
 #
 
-
-def histog(A, B):
-	NA = plt.hist(A, bins=255, alpha=0.5, color='b')
-	AC = plt.hist(B, bins=255, alpha=0.5, color='r')
-	plt.show()
+def histogram(data, labels = None, outfile = None ):
+	if labels == None:
+		labels = [''] * len(data)
+	for i in range(0,len(data)):
+		plt.hist(data[i], bins=255, alpha=0.5, label = labels[i])
+	plt.xlabel('Intensity')
+	plt.ylabel('Number of occurrencies')
+	plt.legend()
+	if outfile == None:
+		plt.show()
+	else:
+		plt.savefig(outfile)
 #
