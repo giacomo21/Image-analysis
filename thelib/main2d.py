98133f7a345e109488c2b22bc5f5a6ae06c91df6
Marco Mina <marco.mina@fbk.eu>
2013-07-15 19:14:00 +0200

    ok

--- thelib/main2d.py
+++ thelib/main2d.py
@@ -1,84 +1,7 @@
 import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-cmap = cm.Greys_r
-
-import loader
-import processing
-import output
-
-# input:
-# input files (list of lists, condition1:[slice1:[img1, img2, ...], slice2:[img3, img4, ...]])
-# input params: condition1 files, condition2 files, roi channel index, molecule channel index
-# 
-
-'''
-Compares 2 conditions
-Assume slices are described by N images. nucleus/molecule concentration (or any molecule to be considered)
-'''
-
-slices1_data = load.load_slices(slices1)
-slices_1_gray = []
-	for i in slices_1:
-		slices_1_gray.append([])
-		slices_1_gray[len(slices_1_gray)-1].append(load.select_channel(i[0], channel = 1))
-		slices_1_gray[len(slices_1_gray)-1].append(load.select_channel(i[1], channel = 0))
-
-	slices2_data = load.load_slices(slices2)
-	slice1_distrib = []
-	for i in slices1_data:
-		slice1_distrib.append(analysis.get_intensity(i[0], i[1]))
-	slice2_distrib = []
-	for i in slices2_data:
-		slice2_distrib.append(analysis.litafnucleus(i[0], i[1]))
-	return(slice1_distrib, slice2_distrib)
-
-def analyze_slices(slices1, slices2):
-	slices1_data = load.load_slices(slices1)
-	slices_1_gray = []
-	for i in slices_1:
-		slices_1_gray.append([])
-		slices_1_gray[len(slices_1_gray)-1].append(load.select_channel(i[0], channel = 1))
-		slices_1_gray[len(slices_1_gray)-1].append(load.select_channel(i[1], channel = 0))
-
-	slices2_data = load.load_slices(slices2)
-	slice1_distrib = []
-	for i in slices1_data:
-		slice1_distrib.append(analysis.get_intensity(i[0], i[1]))
-	slice2_distrib = []
-	for i in slices2_data:
-		slice2_distrib.append(analysis.litafnucleus(i[0], i[1]))
-	return(slice1_distrib, slice2_distrib)
-#
-
-AC = [
-	['data/AC_HSC_CTRL/01_nucleus.tif','data/AC_HSC_CTRL/01_LITAF.tif'],
-	['data/AC_HSC_CTRL/02_nucleus.tif','data/AC_HSC_CTRL/02_LITAF.tif'],
-	['data/AC_HSC_CTRL/03_nucleus.tif','data/AC_HSC_CTRL/03_LITAF.tif'],
-	['data/AC_HSC_CTRL/04_nucleus.tif','data/AC_HSC_CTRL/04_LITAF.tif'],
-	['data/AC_HSC_CTRL/05_nucleus.tif','data/AC_HSC_CTRL/05_LITAF.tif'],
-	['data/AC_HSC_CTRL/06_nucleus.tif','data/AC_HSC_CTRL/06_LITAF.tif']
-	]
-
-NA = [
-	['data/NA_HSC_CTRL/01_nucleus.tif','data/NA_HSC_CTRL/01_LITAF.tif'],
-	['data/NA_HSC_CTRL/02_nucleus.tif','data/NA_HSC_CTRL/02_LITAF.tif'],
-	['data/NA_HSC_CTRL/03_nucleus.tif','data/NA_HSC_CTRL/03_LITAF.tif'],
-	['data/NA_HSC_CTRL/04_nucleus.tif','data/NA_HSC_CTRL/04_LITAF.tif'],
-	['data/NA_HSC_CTRL/05_nucleus.tif','data/NA_HSC_CTRL/05_LITAF.tif'],
-	['data/NA_HSC_CTRL/06_nucleus.tif','data/NA_HSC_CTRL/06_LITAF.tif']
-	]
-
-intensity = analyze_slices(AC, NA)
-
-plt.boxplot([intensity[0][0],intensity[1][0]])
-output_file = 'test.png'
-plt.savefig(output_file)
-
-
-
-import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.cm as cm
+import scipy.stats
 cmap = cm.Greys_r
 
 import loader
@@ -158,17 +81,98 @@ for j in slices_1_intensity:
 	for k in range(0,len(j)):
 		merged_intensity[k] = np.concatenate([merged_intensity[k], j[k]])
 
+LPS500res = slices_1_intensity
+
+
 plt.hist(merged_intensity[0], bins=255, color='r', alpha=0.5)
 plt.hist(merged_intensity[1], bins=255, color='g', alpha=0.5)
 plt.hist(merged_intensity[2], bins=255, color='b', alpha=0.5)
-
-LPS500res = slices_1_intensity
-
+plt.show()
 
 plt.hist(slices_1_intensity[0][0], bins=255, color='r', alpha=0.5)
 plt.hist(slices_1_intensity[0][1], bins=255, color='g', alpha=0.5)
 plt.hist(slices_1_intensity[0][2], bins=255, color='b', alpha=0.5)
 plt.show()
 
+scipy.stats.ranksums(LPS500res[0][0], NAres[2][0])
 
-import scipy.stats
+
+
+
+
+
+# import matplotlib.pyplot as plt
+# import matplotlib.cm as cm
+# cmap = cm.Greys_r
+
+# import loader
+# import processing
+# import output
+
+# # input:
+# # input files (list of lists, condition1:[slice1:[img1, img2, ...], slice2:[img3, img4, ...]])
+# # input params: condition1 files, condition2 files, roi channel index, molecule channel index
+# # 
+
+# '''
+# Compares 2 conditions
+# Assume slices are described by N images. nucleus/molecule concentration (or any molecule to be considered)
+# '''
+
+# slices1_data = load.load_slices(slices1)
+# slices_1_gray = []
+# 	for i in slices_1:
+# 		slices_1_gray.append([])
+# 		slices_1_gray[len(slices_1_gray)-1].append(load.select_channel(i[0], channel = 1))
+# 		slices_1_gray[len(slices_1_gray)-1].append(load.select_channel(i[1], channel = 0))
+
+# 	slices2_data = load.load_slices(slices2)
+# 	slice1_distrib = []
+# 	for i in slices1_data:
+# 		slice1_distrib.append(analysis.get_intensity(i[0], i[1]))
+# 	slice2_distrib = []
+# 	for i in slices2_data:
+# 		slice2_distrib.append(analysis.litafnucleus(i[0], i[1]))
+# 	return(slice1_distrib, slice2_distrib)
+
+# def analyze_slices(slices1, slices2):
+# 	slices1_data = load.load_slices(slices1)
+# 	slices_1_gray = []
+# 	for i in slices_1:
+# 		slices_1_gray.append([])
+# 		slices_1_gray[len(slices_1_gray)-1].append(load.select_channel(i[0], channel = 1))
+# 		slices_1_gray[len(slices_1_gray)-1].append(load.select_channel(i[1], channel = 0))
+
+# 	slices2_data = load.load_slices(slices2)
+# 	slice1_distrib = []
+# 	for i in slices1_data:
+# 		slice1_distrib.append(analysis.get_intensity(i[0], i[1]))
+# 	slice2_distrib = []
+# 	for i in slices2_data:
+# 		slice2_distrib.append(analysis.litafnucleus(i[0], i[1]))
+# 	return(slice1_distrib, slice2_distrib)
+# #
+
+# AC = [
+# 	['data/AC_HSC_CTRL/01_nucleus.tif','data/AC_HSC_CTRL/01_LITAF.tif'],
+# 	['data/AC_HSC_CTRL/02_nucleus.tif','data/AC_HSC_CTRL/02_LITAF.tif'],
+# 	['data/AC_HSC_CTRL/03_nucleus.tif','data/AC_HSC_CTRL/03_LITAF.tif'],
+# 	['data/AC_HSC_CTRL/04_nucleus.tif','data/AC_HSC_CTRL/04_LITAF.tif'],
+# 	['data/AC_HSC_CTRL/05_nucleus.tif','data/AC_HSC_CTRL/05_LITAF.tif'],
+# 	['data/AC_HSC_CTRL/06_nucleus.tif','data/AC_HSC_CTRL/06_LITAF.tif']
+# 	]
+
+# NA = [
+# 	['data/NA_HSC_CTRL/01_nucleus.tif','data/NA_HSC_CTRL/01_LITAF.tif'],
+# 	['data/NA_HSC_CTRL/02_nucleus.tif','data/NA_HSC_CTRL/02_LITAF.tif'],
+# 	['data/NA_HSC_CTRL/03_nucleus.tif','data/NA_HSC_CTRL/03_LITAF.tif'],
+# 	['data/NA_HSC_CTRL/04_nucleus.tif','data/NA_HSC_CTRL/04_LITAF.tif'],
+# 	['data/NA_HSC_CTRL/05_nucleus.tif','data/NA_HSC_CTRL/05_LITAF.tif'],
+# 	['data/NA_HSC_CTRL/06_nucleus.tif','data/NA_HSC_CTRL/06_LITAF.tif']
+# 	]
+
+# intensity = analyze_slices(AC, NA)
+
+# plt.boxplot([intensity[0][0],intensity[1][0]])
+# output_file = 'test.png'
+# plt.savefig(output_file)
