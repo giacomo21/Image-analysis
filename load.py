eb5d42a769cf5f1bb8aeb6ad432468908bf1e054
Marco Mina <marco.mina@fbk.eu>
2013-07-15 09:24:02 +0200

    fixed load.py

--- load.py
+++ load.py
@@ -1,20 +1,59 @@
 import matplotlib.pyplot as plt
 
-def loadimage(filename):
+'''
+Load single image
+Input: filename
+Output: ndarray
+'''
+def load_image(filename):
 	temp = plt.imread(filename)
 	return(temp)
 #
 
-def selectchannel(ndarr, channel = 0):
+'''
+Select only 1 channel from multichannel images
+Input: ndarray (more than 2 dimensions)
+Output: ndarray (2 dimensions)
+'''
+def select_channel(ndarr, channel = 0):
 	temp = ndarr[:,:,channel]
 	return(temp)
 # 
 
-def loadslice(filenames):
+'''
+Load a set of images for the same slice.
+Input: list of image filenames
+Output: list of ndarrayas (in the same order as input data)
+'''
+def load_slice(filenames):
 	temp = []
 	for i in filenames:
-		ndarr = loadimage(i)
-		ndarr = selectchannel(ndarr, 0)
+		ndarr = load_image(i)
+		ndarr = select_channel(ndarr, 0) # move outside
 		temp.append(ndarr)
 	return(temp)
+#
+
+'''
+Load several slices.
+Input: list of lists of image filenames
+Output: list of lists of ndarrayas (in the same order as input data)
+'''
+def load_slices(slices):
+	slices_data = []
+	for i in slices:
+		slices_data.append(load_slice(i))
+	return(slices_data)
+#
+
+'''
+Load stack of slices (multiple images per slice supported)
+Input: list of lists of image filenames
+Output: list of lists of ndarrayas (in the same order as input data)
+'''
+# def loadstack(filenames):
+# 	temp = []
+# 	for i in filenames:
+# 		temp.append(loadslice[i])
+# 	return(temp)
 #
--- marco_code.py
+++ marco_code.py
@@ -5,11 +5,49 @@ cmap = cm.Greys_r
 import load
 import analysis
 
-AC = ['../data/Alisi/Hepatic_stellate/AC_HSC_CTRL/03_nucleus.tif','../data/Alisi/Hepatic_stellate/AC_HSC_CTRL/03_LITAF.tif']
-NA = ['../data/Alisi/Hepatic_stellate/NA_HSC_CTRL/03_nucleus.tif','../data/Alisi/Hepatic_stellate/NA_HSC_CTRL/03_LITAF.tif']
+'''
+Comapares 2 slices
+Assume slices are described by N images. The first is the nucleus, the second LITAF (or any molecule to be considered)
+'''
+def analyze_2slices(slice1, slice2):
+	slice1_data= load.loadslice(slice1)
+	slice2_data= load.loadslice(slice2)
+	slice1_concentration = analysis.litafnucleus(slice1_data[0], slice1_data[1])
+	slice2_concentration = analysis.litafnucleus(slice2_data[0], slice2_data[1])
+	return(slice1_concentration, slice2_concentration)
 
-ACdata= load.loadslice(AC)
-NAdata= load.loadslice(NA)
+'''
+Comapares 2 sets of slices
+Assume slices are described by N images. The first is the nucleus, the second LITAF (or any molecule to be considered)
+'''
+def analyze_slices(slices1, slices2):
+	slices1_data = load.load_slices(slices1)
+	slices2_data = load.load_slices(slices2)
+	slice1_distrib = []
+	for i in slices1_data:
+		slice1_distrib.append(analysis.litafnucleus(i[0], i[1]))
+	slice2_distrib = []
+	for i in slices2_data:
+		slice2_distrib.append(analysis.litafnucleus(i[0], i[1]))
+	return(slice1_distrib, slice2_distrib)
+#
 
-ACres = analysis.litafnucleus(ACdata[0], ACdata[1])
-NAres = analysis.litafnucleus(NAdata[0], NAdata[1])
+AC = [
+	['../data/Alisi/Hepatic_stellate/AC_HSC_CTRL/01_nucleus.tif','../data/Alisi/Hepatic_stellate/AC_HSC_CTRL/01_LITAF.tif'],
+	['../data/Alisi/Hepatic_stellate/AC_HSC_CTRL/02_nucleus.tif','../data/Alisi/Hepatic_stellate/AC_HSC_CTRL/02_LITAF.tif'],
+	['../data/Alisi/Hepatic_stellate/AC_HSC_CTRL/03_nucleus.tif','../data/Alisi/Hepatic_stellate/AC_HSC_CTRL/03_LITAF.tif'],
+	['../data/Alisi/Hepatic_stellate/AC_HSC_CTRL/04_nucleus.tif','../data/Alisi/Hepatic_stellate/AC_HSC_CTRL/04_LITAF.tif'],
+	['../data/Alisi/Hepatic_stellate/AC_HSC_CTRL/05_nucleus.tif','../data/Alisi/Hepatic_stellate/AC_HSC_CTRL/05_LITAF.tif'],
+	['../data/Alisi/Hepatic_stellate/AC_HSC_CTRL/06_nucleus.tif','../data/Alisi/Hepatic_stellate/AC_HSC_CTRL/06_LITAF.tif']
+	]
+
+NA = [
+	['../data/Alisi/Hepatic_stellate/NA_HSC_CTRL/01_nucleus.tif','../data/Alisi/Hepatic_stellate/NA_HSC_CTRL/01_LITAF.tif'],
+	['../data/Alisi/Hepatic_stellate/NA_HSC_CTRL/02_nucleus.tif','../data/Alisi/Hepatic_stellate/NA_HSC_CTRL/02_LITAF.tif'],
+	['../data/Alisi/Hepatic_stellate/NA_HSC_CTRL/03_nucleus.tif','../data/Alisi/Hepatic_stellate/NA_HSC_CTRL/03_LITAF.tif'],
+	['../data/Alisi/Hepatic_stellate/NA_HSC_CTRL/04_nucleus.tif','../data/Alisi/Hepatic_stellate/NA_HSC_CTRL/04_LITAF.tif'],
+	['../data/Alisi/Hepatic_stellate/NA_HSC_CTRL/05_nucleus.tif','../data/Alisi/Hepatic_stellate/NA_HSC_CTRL/05_LITAF.tif'],
+	['../data/Alisi/Hepatic_stellate/NA_HSC_CTRL/06_nucleus.tif','../data/Alisi/Hepatic_stellate/NA_HSC_CTRL/06_LITAF.tif']
+	]
+	
+analyze_slices(AC, NA)
