e936108e7decf9fdfb33b4110d6928cd53794993
Nicola Calzà <calzanicola@gmail.com>
2013-07-16 00:19:10 +0200

    v2

--- nicola_thelib/nicola_output.py
+++ nicola_thelib/nicola_output.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+import numpy as np
 
 def plot(data):
 	cmap = cm.Greys_r
@@ -7,9 +8,32 @@ def plot(data):
 	plt.show()
 #
 
+def histogram(data, labels = None, outfile = None):
+	if labels == None:
+		labels = [''] * len(data)
+	for i in range(0,len(data)):
+		plt.hist(data[i], bins=255, alpha=0.5, label = labels[i])
+	plt.xlabel('Intensity')
+	plt.ylabel('Number of occurrencies')
+	plt.legend()
+	if outfile == None:
+		plt.show()
+	else:
+		plt.savefig(outfile)
+#
 
-def histog(A, B):
-	NA = plt.hist(A, bins=255, alpha=0.5, color='b')
-	AC = plt.hist(B, bins=255, alpha=0.5, color='r')
-	plt.show()
+def boxplot(x, labels = None, outfile = None, xlab = '', ylab = ''):
+	if labels == None:
+		labels = [''] * len(x)
+	plt.boxplot(x)
+	plt.xticks(range(1,len(x)+1), labels)
+	# y=range(0,256)
+	# plt.yticks(y, y)
+	plt.ylim(0, 256)
+	plt.xlabel(xlab)
+	plt.ylabel(ylab)
+	if outfile == None:
+		plt.show()
+	else:
+		plt.savefig(outfile)
 #
