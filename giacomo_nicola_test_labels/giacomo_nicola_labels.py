07faad9d0b6534c1cebd1711b0851fb6ef99666e
Nicola Calzà <calzanicola@gmail.com>
2013-07-17 16:45:57 +0200

    A

--- giacomo_nicola_test_labels/giacomo_nicola_labels.py
+++ giacomo_nicola_test_labels/giacomo_nicola_labels.py
@@ -3,19 +3,21 @@ def labelfunc(condition, condition_name = None, condition_name_slice = None, con
 	if not condition_name == None:
 		for j in condition:
 			for i in j:
-				label_cond_name = condition_name * len(j)
+				label_cond_name = condition_name
 	else:
 		for j in condition:
-			for i in j:
+#			for i in j:
 				label_cond_name.append(str(condition.index(j)))
 	label_cond_name_slice = []
 	if not condition_name_slice == None:
 		for i in condition_name_slice:
 			for j in label_cond_name:
-				label_cond_name_slice = j + '_' + i
+				label_cond_name_slice.append(j + '_' + i)
 	else:
 		for j in label_cond_name:
-			label_cond_name_slice = j + '_' + str(label_cond_name.index(j))
+			for i in condition:
+				for y in i:
+					label_cond_name_slice.append(j + '_' + str(i.index(y)))
 
 #	if not condition_channel == None:
 
