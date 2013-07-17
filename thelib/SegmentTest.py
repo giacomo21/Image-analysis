a5f2f891d1f8531e719c203b749c93ce962a4489
Nikhil96 <nikhilkotha199623@gmail.com>
2013-07-17 16:25:07 +0200

    òsdjfk

---
+++ thelib/SegmentTest.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import numpy as np
+import scipy
+import pylab
+import pymorph
+import mahotas
+from scipy import ndimage
+import matplotlib.pyplot     as     plt
+
+
+
+dna = plt.imread('/home/nikhil/Desktop/Alisi Data/3D/CCK_8_18/8_Z8_C2.tif')
+pylab.imshow(dna)
+pylab.show()
