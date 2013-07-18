5cae5261cf3398ceea8e2461cb047ce336de37f2
Marco Mina <marco.mina@fbk.eu>
2013-07-18 11:20:39 +0200

    ok

--- thelib/main3D.py
+++ thelib/main3D.py
@@ -13,13 +13,18 @@ from scipy import ndimage
 import pymorph
 
 #Data of HSC60x
-'''
+
 dataHSC = a.compare_molecule_distribution([c.NAimgs, c.ACimgs],
 		nucleus_index = 0, molecule_index = 1,
 		nucleus_channel = 0, molecule_channel = 0,
 		nucleus_fill_holes = True, nucleus_otsu = True,
 		molecule_fill_holes = False, molecule_otsu = False)
-'''
+
+dataHSC2 = a.compare_molecule_distribution([c.NA2imgs, c.AC2imgs],
+		nucleus_index = 0, molecule_index = 1,
+		nucleus_channel = 0, molecule_channel = 0,
+		nucleus_fill_holes = True, nucleus_otsu = True,
+		molecule_fill_holes = False, molecule_otsu = False)
 
 dataHEP = a.compare_molecule_distribution([c.CCKimgs, c.NCKimgs],
 		nucleus_index = 0, molecule_index = 1,
@@ -27,51 +32,50 @@ dataHEP = a.compare_molecule_distribution([c.CCKimgs, c.NCKimgs],
 		nucleus_fill_holes = True, nucleus_otsu = True,
 		molecule_fill_holes = False, molecule_otsu = False)
 
-A = dataHEP[0]['slices_mask'][10][0]
-rmax = pymorph.regmax(A)
-seeds, nr_nuclei = ndimage.label(rmax)
-print nr_nuclei
-T = mahotas.thresholding.otsu(A)
-C = A.copy()
-if T < 1:
-	C[ C <= T ] = 0
-	C[ C > T ] = 1
-else:
-	C[ C < T ] = 0
-	C[ C >= T ] = 1
-filled = scipy.ndimage.morphology.binary_fill_holes(C)
-filled = filled.astype(np.uint8)
-	
-dist = ndimage.distance_transform_edt(filled > T)
-dist = dist.max() - dist
-dist -= dist.min()
-dist = dist/float(dist.ptp()) * 255
-dist = dist.astype(np.uint8)
-		
-nuclei = pymorph.cwatershed(dist, seeds)
-plt.imshow(nuclei)
-plt.show()
-find = mahotas.label(nuclei)
-print find[1]
-
-
 #3D Construction Image Details of NA60x
-#b.TDAnalysis(dataHSC[0]['slices_mask'], 39, '/home/nikhil/Image-analysis/Nikhil_Trials/NAFullData.txt', 'w')
-#b.CanNuc(dataHSC[0]['slices_mask'], 39, '/home/nikhil/Image-analysis/Nikhil_Trials/NAFullData.txt', 'a')
-#b.findlitaf(dataHSC[0]['slices_mask'], 39, '/home/nikhil/Image-analysis/Nikhil_Trials/NAFullData.txt', 'a')
+
+b.LIN(dataHSC[0]['slices_gray'], 38, '/home/nikhil/Image-analysis/Nikhil_Trials/NAFullData.txt', 'w')
+b.SNUC(dataHSC[0]['slices_mask'], 38, '/home/nikhil/Image-analysis/Nikhil_Trials/NAFullData.txt', 'a')
+b.LIT(dataHSC[0]['slices_mask'], 38, '/home/nikhil/Image-analysis/Nikhil_Trials/NAFullData.txt', 'a')
+b.LIA(dataHSC[0]['slices_mask'], 38, '/home/nikhil/Image-analysis/Nikhil_Trials/NAIA.txt', 'w')
+
 
 #3D Construction Image Details of AC60x
-#b.TDAnalysis(dataHSC[1]['slices_mask'], 27, '/home/nikhil/Image-analysis/Nikhil_Trials/ACFullData.txt', 'w')
-#b.CanNuc(dataHSC[1]['slices_mask'], 27, '/home/nikhil/Image-analysis/Nikhil_Trials/ACFullData.txt', 'a')
-#b.findlitaf(dataHSC[1]['slices_mask'], 27, '/home/nikhil/Image-analysis/Nikhil_Trials/ACFullData.txt', 'a')
+
+b.LIN(dataHSC[1]['slices_gray'], 26, '/home/nikhil/Image-analysis/Nikhil_Trials/ACFullData.txt', 'w')
+b.SNUC(dataHSC[1]['slices_mask'], 26, '/home/nikhil/Image-analysis/Nikhil_Trials/ACFullData.txt', 'a')
+b.LIT(dataHSC[1]['slices_mask'], 26, '/home/nikhil/Image-analysis/Nikhil_Trials/ACFullData.txt', 'a')
+b.LIA(dataHSC[1]['slices_mask'], 26, '/home/nikhil/Image-analysis/Nikhil_Trials/ACIA.txt', 'w')
+
+
+#3D Construction Image Details of NA60x2
+
+b.LIN(dataHSC2[0]['slices_gray'], 26, '/home/nikhil/Image-analysis/Nikhil_Trials/NA2FullData.txt', 'w')
+b.SNUC(dataHSC2[0]['slices_mask'], 26, '/home/nikhil/Image-analysis/Nikhil_Trials/NA2FullData.txt', 'a')
+b.LIT(dataHSC2[0]['slices_mask'], 26, '/home/nikhil/Image-analysis/Nikhil_Trials/NA2FullData.txt', 'a')
+b.LIA(dataHSC2[0]['slices_mask'], 26, '/home/nikhil/Image-analysis/Nikhil_Trials/NA2IA.txt', 'w')
+
+
+#3D Construction Image Details of AC60x2
+
+b.LIN(dataHSC2[1]['slices_gray'], 44, '/home/nikhil/Image-analysis/Nikhil_Trials/AC2FullData.txt', 'w')
+b.SNUC(dataHSC2[1]['slices_mask'], 44, '/home/nikhil/Image-analysis/Nikhil_Trials/AC2FullData.txt', 'a')
+b.LIT(dataHSC2[1]['slices_mask'], 44, '/home/nikhil/Image-analysis/Nikhil_Trials/AC2FullData.txt', 'a')
+b.LIA(dataHSC2[1]['slices_mask'], 44, '/home/nikhil/Image-analysis/Nikhil_Trials/AC2IA.txt', 'w')
+
 
 #3D Construction Image Details of CCK
-#b.TDAnalysis(dataHEP[0]['slices_mask'], 30, '/home/nikhil/Image-analysis/Nikhil_Trials/CCKFullData.txt', 'w')
-#b.CanNuc(dataHEP[0]['slices_mask'], 1, '/home/nikhil/Image-analysis/Nikhil_Trials/CCKFullData.txt', 'a')
-#b.findlitaf(dataHEP[0]['slices_mask'], 30, '/home/nikhil/Image-analysis/Nikhil_Trials/CCKFullData.txt', 'a')
+
+b.LIN(dataHEP[0]['slices_gray'], 29, '/home/nikhil/Image-analysis/Nikhil_Trials/CCKFullData.txt', 'w')
+b.MNUC(dataHEP[0]['slices_mask'], 29, '/home/nikhil/Image-analysis/Nikhil_Trials/CCKFullData.txt', 'a')
+b.LIT(dataHEP[0]['slices_mask'], 29, '/home/nikhil/Image-analysis/Nikhil_Trials/CCKFullData.txt', 'a')
+b.LIA(dataHEP[0]['slices_mask'], 29, '/home/nikhil/Image-analysis/Nikhil_Trials/CCKIA.txt', 'w')
+
 
 #3D Construction Image Details of NCK
-#b.TDAnalysis(dataHEP[1]['slices_mask'], 30, '/home/nikhil/Image-analysis/Nikhil_Trials/NCKFullData.txt', 'w')
-#b.CanNuc(dataHEP[1]['slices_mask'], 30, '/home/nikhil/Image-analysis/Nikhil_Trials/NCKFullData.txt', 'a')
-#b.findlitaf(dataHEP[1]['slices_mask'], 30, '/home/nikhil/Image-analysis/Nikhil_Trials/NCKFullData.txt', 'a')
+
+b.LIN(dataHEP[1]['slices_gray'], 29, '/home/nikhil/Image-analysis/Nikhil_Trials/NCKFullData.txt', 'w')
+b.MNUC(dataHEP[1]['slices_mask'], 29, '/home/nikhil/Image-analysis/Nikhil_Trials/NCKFullData.txt', 'a')
+b.LIT(dataHEP[1]['slices_mask'], 29, '/home/nikhil/Image-analysis/Nikhil_Trials/NCKFullData.txt', 'a')
+b.LIA(dataHEP[1]['slices_mask'], 29, '/home/nikhil/Image-analysis/Nikhil_Trials/NCKIA.txt', 'w')
 
--- thelib/processing3D.py
+++ thelib/processing3D.py
@@ -14,10 +14,8 @@ def get_molecule_pos(data):
 		XY = np.vstack((SC[0], SC[1], data['slices_mask'][i][temp]))
 	return XY
 
-def TDAnalysis(datatype, maxrange, outputfile, outputfiletype):
-	NC = 0
+def LIA(datatype, maxrange, outputfile, outputfiletype):
 	h = open(outputfile, outputfiletype)
-	h.write(str(datatype[0][0].shape[0]) + '\t' + str(datatype[0][0].shape[1]) + '\n')
 	for i in range(0, maxrange):
 		A = datatype[i][0]
 		T = mahotas.thresholding.otsu(A)
@@ -31,9 +29,31 @@ def TDAnalysis(datatype, maxrange, outputfile, outputfiletype):
 		filled = scipy.ndimage.morphology.binary_fill_holes(C)
 		filled = filled.astype(np.uint8)
 		SC = np.where(filled == 1)
+		GRAY = datatype[i][1][SC[0], SC[1]]
+		if len(SC[0]) >= 1:
+			if len(SC[1]) >= 1:
+				h.write('Stack Number: ' + str(i) +'\n')
+				h.write('Intensity Values: ' + str(GRAY) + '\n' + '\n')
+	h.close()
+
+def LIN(datatype, maxrange, outputfile, outputfiletype):
+	NC = 0
+	h = open(outputfile, outputfiletype)
+	h.write(str(datatype[0][0].shape[0]) + '\t' + str(datatype[0][0].shape[1]) + '\n')
+	for i in range(0, maxrange + 1):
+		A = datatype[i][0]
+		T = mahotas.thresholding.otsu(A)
+		C = A.copy()
+		if T < 1:
+			C[ C <= T ] = 0
+			C[ C > T ] = 1
+		else:
+			C[ C < T ] = 0
+			C[ C >= T ] = 1
+		filled = scipy.ndimage.morphology.binary_fill_holes(C)
+		filled = filled.astype(np.uint8)
+		SC = np.where(filled == 1)
 		NC += len(SC[0])
-		#print 'X-coordinates:                          ', SC[0], '\n'
-		#print 'Y-coordinates:                          ', SC[1], '\n'
 		GRAY = datatype[i][1][SC[0], SC[1]]
 		#if len(SC[0]) >= 1:
 		#	if len(SC[1]) >= 1:
@@ -41,10 +61,7 @@ def TDAnalysis(datatype, maxrange, outputfile, outputfiletype):
 		RED = GRAY.copy()
 		GREEN = GRAY.copy()
 		BLUE = GRAY.copy()
-		print SC[0].shape
-		print SC[1].shape
 		XY = np.vstack((SC[0], SC[1], [i*5]*len(RED), RED, GREEN, BLUE))
-		#print XY
 		for p in range(0, len(XY[0])):
 			for yel in range(0, len(XY)):
 				h.write(str(XY[yel][p]) + '\t')
@@ -54,10 +71,38 @@ def TDAnalysis(datatype, maxrange, outputfile, outputfiletype):
 	h.close()
 	return 
 
-def CanNuc(datatype, maxrange, outputfile, outputfiletype):
+def SNUC(datatype, maxrange, outputfile, outputfiletype):
 	h = open(outputfile, outputfiletype)
 	TC = 0	
-	for i in range(0, maxrange):
+	for i in range(0, maxrange + 1):
+		A = datatype[i][0]
+		T = mahotas.thresholding.otsu(A)
+		C = A.copy()
+		if T < 1:
+			C[ C <= T ] = 0
+			C[ C > T ] = 1
+		else:
+			C[ C < T ] = 0
+			C[ C >= T ] = 1
+		filled = scipy.ndimage.morphology.binary_fill_holes(C)
+		filled = filled.astype(np.uint8)
+		edges = filter.canny(filled, sigma=1)
+		edges = edges.astype(np.uint8)
+		edges = np.where(edges == 1)
+		TC += len(edges[0])
+		XY1 = np.vstack((edges[0], edges[1], [i*5]*len(edges[0])))
+		for p in range(0, len(XY1[0])):
+			for yel in range(0, len(XY1)):
+				h.write(str(XY1[yel][p]) + '\t')
+			h.write('\n')
+	h.write(str(TC) + '\n')
+	h.write('.' + '\n')
+	h.close()
+	
+def MNUC(datatype, maxrange, outputfile, outputfiletype):
+	h = open(outputfile, outputfiletype)
+	TC = 0	
+	for i in range(0, maxrange + 1):
 		A = datatype[i][0]
 		rmax = pymorph.regmax(A)
 		seeds, nr_nuclei = ndimage.label(rmax)
@@ -79,21 +124,15 @@ def CanNuc(datatype, maxrange, outputfile, outputfiletype):
 		dist = dist.astype(np.uint8)
 		
 		nuclei = pymorph.cwatershed(dist, seeds)
-		plt.imshow(nuclei)
-		plt.show()
 		find = mahotas.label(nuclei)
-		h.write('Stack number: ' + str(i) +'\n')
 		for n in range(0, find[1] + 1):
 			CD = np.where(find[0] == n)
 			XY1 = np.vstack((CD[0], CD[1]))
 			edges = filter.canny(XY1, sigma=1)
 			edges = edges.astype(np.uint8)
 			edges = np.where(edges == 1)
-			TC += len(edges[0])
-						
-			h.write('Cell number: ' + str(n) + '\n')
-		#TC += len(edges1[0])
-		#XY1 = np.vstack((edges1[0], edges1[1], [i*5]*len(edges1[0])))
+			TC += len(CD[0])
+			XY1 = np.vstack((CD[0], CD[1], [i*5]*len(CD[0]), [n]*len(CD[0])))			
 			for p in range(0, len(XY1[0])):
 				for yel in range(0, len(XY1)):
 					h.write(str(XY1[yel][p]) + '\t')
@@ -105,10 +144,10 @@ def CanNuc(datatype, maxrange, outputfile, outputfiletype):
 
 
 
-def findlitaf(datatype, maxrange, outputfile, outputfiletype):
+def LIT(datatype, maxrange, outputfile, outputfiletype):
 	h = open(outputfile, outputfiletype)	
 	TC = 0
-	for i in range(0, maxrange):
+	for i in range(0, maxrange + 1):
 		A = datatype[i][0]
 		LI = np.where(A > 0)
 		TC += len(LI[0])		
