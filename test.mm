bbbc610d061881996fd07806de5e653651739ace
Marco Mina <marco.mina@fbk.eu>
2013-07-22 10:02:55 +0200

    ok

---
+++ test.mm
@@ -0,0 +1,101 @@
+<map version="freeplane 1.2.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node LOCALIZED_TEXT="new_mindmap" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1283093380553"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="0"/>
+<node TEXT="data" POSITION="right" ID="ID_1948981942" CREATED="1373439847756" MODIFIED="1373439851761">
+<edge COLOR="#00007c"/>
+<node TEXT="bioimaging" ID="ID_1278156993" CREATED="1373439852101" MODIFIED="1373439869589">
+<node TEXT="Anna Alisi" ID="ID_1497667558" CREATED="1373439861317" MODIFIED="1373439864279">
+<node TEXT="Hepatocytes" ID="ID_810062803" CREATED="1373439934974" MODIFIED="1373439973065">
+<node TEXT="primary cells" ID="ID_650321488" CREATED="1373439993444" MODIFIED="1373439995760"/>
+<node TEXT="epatocites in NAFLD" ID="ID_859407571" CREATED="1373377731840" MODIFIED="1373377739731"/>
+</node>
+<node TEXT="Hepatic stellate" ID="ID_276761122" CREATED="1373439973909" MODIFIED="1373439980065">
+<node TEXT="primary cells" ID="ID_872664928" CREATED="1373439989444" MODIFIED="1373439992384"/>
+<node TEXT="normal" ID="ID_162799506" CREATED="1373377049861" MODIFIED="1373377061581">
+<node TEXT="no high expression of Litaf" ID="ID_76537326" CREATED="1373377061890" MODIFIED="1373377074197"/>
+<node TEXT="no Litaf in nuclei" ID="ID_1131195632" CREATED="1373377062929" MODIFIED="1373377069244"/>
+</node>
+<node TEXT="LPS-induced" ID="ID_490185280" CREATED="1373377078601" MODIFIED="1373377088012">
+<node TEXT="higher Litaf expression" ID="ID_579717936" CREATED="1373377088514" MODIFIED="1373377094044"/>
+<node TEXT="Litaf phosporilated and present in nuclei" ID="ID_1363198479" CREATED="1373377094849" MODIFIED="1373377120491"/>
+<node TEXT="aumento di COL6" ID="ID_388430159" CREATED="1373377200067" MODIFIED="1373377202701"/>
+<node TEXT="aumento di aSMA" ID="ID_1675808705" CREATED="1373377244625" MODIFIED="1373377255711">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="80" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1860333254" STARTINCLINATION="117;0;" ENDINCLINATION="117;0;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+</node>
+<node TEXT="qualitative analysis" ID="ID_1985708385" CREATED="1373377259200" MODIFIED="1373377269268"/>
+</node>
+<node TEXT="there is some noise" ID="ID_278120141" CREATED="1373377124056" MODIFIED="1373377128356"/>
+<node TEXT="Litaf production regulated by P53" ID="ID_1935734825" CREATED="1373377139191" MODIFIED="1373377149011"/>
+<node TEXT="Litaf fphosphorilation regulated by P38 MAPK" ID="ID_1319986945" CREATED="1373377149376" MODIFIED="1373377163035"/>
+<node TEXT="transaction from epithelial to mesenchymal phenotype" ID="ID_1860333254" CREATED="1373374966736" MODIFIED="1373377234845"/>
+<node TEXT="aumento citochine fibrogeniche e proinflammatorie" ID="ID_1616559783" CREATED="1373375610112" MODIFIED="1373375619140"/>
+<node TEXT="Litaf/aSMA in primary stellate cells" ID="ID_1083196325" CREATED="1373377025893" MODIFIED="1373377717531"/>
+<node TEXT="litaf in cell lines" ID="ID_1195156084" CREATED="1373377183537" MODIFIED="1373377191829"/>
+</node>
+<node TEXT="LX-2" ID="ID_818553392" CREATED="1373439980477" MODIFIED="1373439985128">
+<node TEXT="cell lines" ID="ID_580661934" CREATED="1373439985933" MODIFIED="1373439988097"/>
+<node TEXT="litaf already present in non treated lines" ID="ID_1208354752" CREATED="1373377276649" MODIFIED="1373377289707"/>
+<node TEXT="normal" ID="ID_737809154" CREATED="1373377290785" MODIFIED="1373377292067">
+<node TEXT="no Litaf in nuclei" ID="ID_1361569637" CREATED="1373377297441" MODIFIED="1373377301051"/>
+</node>
+<node TEXT="LPS-induced" ID="ID_1134817150" CREATED="1373377292376" MODIFIED="1373377296540">
+<node TEXT="Litaf in nuclei" ID="ID_554234281" CREATED="1373377297441" MODIFIED="1373377307884"/>
+</node>
+</node>
+<node TEXT="hepatic steatosis" ID="ID_108619425" CREATED="1373528670731" MODIFIED="1373528697476"/>
+</node>
+<node TEXT="Daniele Arosio" ID="ID_629192979" CREATED="1373439870476" MODIFIED="1373439876399"/>
+<node TEXT="Steven Altschuler and Lani Wu" ID="ID_299753497" CREATED="1373439878109" MODIFIED="1373439908295"/>
+</node>
+</node>
+</node>
+</map>
